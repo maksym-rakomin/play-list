bdbc7854be9e63d14f4d2546425205a4bb63a721
maksym-rakomin <49858760+maksym-rakomin@users.noreply.github.com>
2019-12-27 19:58:16 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,124 @@
+;(function (){
+    'use strict';
+    //Список предлагаемых песен
+    let playList = [
+        {
+         author: "LED ZEPPELIN",
+         song:"STAIRWAY TO HEAVEN"
+        },
+        {
+         author: "QUEEN",
+         song:"BOHEMIAN RHAPSODY"
+        },
+        {
+         author: "LYNYRD SKYNYRD",
+         song:"FREE BIRD"
+        },
+        {
+         author: "DEEP PURPLE",
+         song:"SMOKE ON THE WATER"
+        },
+        {
+         author: "JIMI HENDRIX",
+         song:"ALL ALONG THE WATCHTOWER"
+        },
+        {
+         author: "AC/DC",
+         song:"BACK IN BLACK"
+        },
+        {
+         author: "QUEEN",
+         song:"WE WILL ROCK YOU"
+        },
+        {
+         author: "METALLICA",
+         song:"ENTER SANDMAN"
+        }
+       ],
+       newPlayList = [];
+
+    let enterComplitedPlayList = document.getElementById('check-play-list-complite'),
+        authorInput            = document.getElementById('author'),
+        songInput              = document.getElementById('song'),
+        addBtn                 = document.getElementById('addBtn'),
+        createBtn              = document.getElementById('createBtn');
+
+        // Слушаем событие и добавляем готовый список при клике
+    enterComplitedPlayList.addEventListener('click', function(e) {
+
+        let lists = document.getElementById('lists');
+
+        lists.innerHTML = "";
+
+        playList.forEach(elem => {
+            let liTrack = document.createElement('li');
+            liTrack.classList.add('track');
+            liTrack.innerHTML = `
+                <span class="author">${elem.author} - </span>
+                <span class="song">"${elem.song}"</span>
+            `;
+
+            lists.append(liTrack);
+        });
+    });
+        //Проверяем заполнен ли инпут для заполнения автора
+    authorInput.addEventListener('input', function() {
+
+            if (authorInput.value.length && songInput.value.length) {
+                addBtn.removeAttribute('disabled');
+            } else {
+                addBtn.setAttribute('disabled', true);
+            }
+    });
+        //Проверяем заполнен ли инпут для заполнения названия песни
+    songInput.addEventListener('input', function() {
+
+            if (authorInput.value.length && songInput.value.length) {
+                addBtn.removeAttribute('disabled');
+            } else {
+                addBtn.setAttribute('disabled', true);
+            }
+    });
+        //При нажатии на кнопку "addBtn" добавляем введенные данные в обьект и в новый массив с песнями
+    addBtn.addEventListener('click', function(e){
+        let obj = {};
+        
+        obj.author = authorInput.value;
+        obj.song = songInput.value;
+        
+        newPlayList.push(obj);
+        
+        authorInput.value = "";
+        songInput.value = "";
+           
+        if (newPlayList.length){
+            createBtn.style.display="block";
+        }
+        addBtn.setAttribute('disabled', true);
+    });
+    
+        //Добавляем новый массив с песнями в HTML
+    createBtn.addEventListener('click', function(e) {
+
+        let lists = document.getElementById('lists');
+        
+        lists.innerHTML = "";
+       
+        newPlayList.forEach(elem => {
+            let liTrack = document.createElement('li');
+
+            liTrack.classList.add('track');
+            liTrack.innerHTML = `
+                <span class="author">${elem.author} - </span>
+                <span class="song">"${elem.song}"</span>
+            `;
+             lists.append(liTrack);
+         });
+
+         createBtn.style.display="none";
+
+         newPlayList = [];
+    });
+
+})();
+
